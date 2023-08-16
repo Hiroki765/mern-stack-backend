95328d04a14bcf19c274d70d8da54776fcdd6705
{Hiroki765} <{tech.h.goto.it@gmail.com}>
2023-08-16 21:14:09 +0900

    CRUD complete.

--- index.js
+++ index.js
@@ -3,23 +3,65 @@ const app = express();
 app.use(express.urlencoded({extended: true}));  // POSTリクエストの解析用
 app.use(express.json());                        // POSTリクエストの解析用
 const connectDB = require("./utils/database");
+const { ItemModel } = require("./utils/schemaModels");
 
 
-app.get("/", (req, res) => {
-    return res.status(200).json("こんにちは");
-});
 
 // ITEM functions
 // Create Item 
-app.post("/item/create", (req,res) => {
-    connectDB();
-    return res.status(200).json("こんにちは");
+app.post("/item/create", async(req,res) => {
+    try{
+        await connectDB();
+        await ItemModel.create(req.body);
+        return res.status(200).json({message: "アイテム作成成功"});
+    }catch(err){
+        return res.status(400).json({message: "アイテム作成失敗"});
+    }
 });
 // Read All Items
-// Read Single Item
-// Update Item
-// Delete Item
+app.get("/", async(req, res) => {
+    try{
+        await connectDB();
+        const allItems = await ItemModel.find();
+        await console.log(allItems);
+        return res.status(200).json({message: "オールアイテム読み取り成功", allItems: allItems});
+    }catch(err){
+        return res.status(400).json({message: "オールアイテム読み取り失敗"});
+    }
 
+});
+// Read Single Item
+app.get("/item/:id", async(req, res) =>{
+    try{
+        await connectDB();
+        const singleItem = await ItemModel.findById(req.params.id);
+        return res.status(200).json({message: "シングルアイテム読み取り成功"})
+    }catch(err){
+        return res.status(400).json({message: "シングルアイテム読み取り失敗"})
+    };
+});
+// Update Item
+app.put("/item/update/:id", async(req, res) => {
+    try{
+        await connectDB();
+        await ItemModel.updateOne({_id: req.params.id}, req.body);
+        return res.status(200).json({message: "アイテム編集成功"});
+    }catch(err){
+        return res.status(400).json({message: "アイテム編集失敗"});
+    }
+});
+// Delete Item
+app.delete("/item/delete/:id", async(req, res) => {
+    try {
+        await connectDB();
+        await ItemModel.deleteOne({_id: req.params.id});
+        return res.status(200).json({message: "アイテム削除成功"});
+    } catch (err) {
+        return res.status(400).json({message: "アイテム削除失敗"});
+    }
+
+
+});
 // USER functions
 // Register User
 // Login User
--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async() => {
-    await mongoose.connect(process.env.MONGOOSE_API)
+    await mongoose.connect(process.env.MONGOOSE_SID_API)
     .then(() => {
         console.log("Success:Connected to MongoDB");
     })
